b68a247c6552c22b7ce61030487962c840f7e606
haiderfazal66 <haider.fazal@stu.ocsb.ca>
2025-01-16 14:04:34 +0000

    m

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+/**
+ * ICS3 - Mr. J 🐠
+ * RICH SUMMATIVE TASK 2024-25 S1
+ *
+ * Description:
+ * 
+ * Author:
+ **/ 
+
+
+'use strict';
+
+// A very accurate rounding function
+function round(value, decimals) {
+    let multiplier = 10**decimals;
+    return Math.round((value + Number.EPSILON) * multiplier) / multiplier;
+}
+
+// Get a random number from min to max
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* 
+    Sleep the code for a certain number of ms
+    NOTE: Any function that will use this must be declared with async and
+    this sleep function called with "await sleep(x)" where x is a number of ms
+*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const themes = {
     school: ["school", "book", "class", "pencil", "teacher", "student", "desk", "chalk", "board", "notebook", "eraser", "lunch", "recess", "backpack", "homework", "principal", "cafeteria", "gym", "library", "playground"],
     sports: ["soccer", "basketball", "baseball", "tennis", "cricket", "hockey", "football", "volleyball", "golf", "swimming", "running", "cycling", "boxing", "wrestling", "skiing", "surfing", "skating", "gymnastics", "archery", "rowing"],
