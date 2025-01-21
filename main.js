905f192ce7675ffdb5401fd3ec15c10a477911c7
haiderfazal66 <haider.fazal@stu.ocsb.ca>
2025-01-21 13:58:49 +0000

    m

--- main.js
+++ main.js
@@ -10,30 +10,22 @@
 
 'use strict';
 
-// A very accurate rounding function
+// Function to round a number to a specified number of decimals
 function round(value, decimals) {
-    let multiplier = 10**decimals;
-    return Math.round((value + Number.EPSILON) * multiplier) / multiplier;
+    return Number(value.toFixed(decimals));
 }
 
-// Get a random number from min to max
+// Function to get a random integer between min and max (inclusive)
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/* 
-    Sleep the code for a certain number of ms
-    NOTE: Any function that will use this must be declared with async and
-    this sleep function called with "await sleep(x)" where x is a number of ms
-*/
+// Function to pause execution for a specified number of milliseconds
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+// Define the theme and their words
 const themes = {
     school: ["school", "book", "class", "pencil", "teacher", "student", "desk", "chalk", "board", "notebook", "eraser", "lunch", "recess", "backpack", "homework", "principal", "cafeteria", "gym", "library", "playground"],
     sports: ["soccer", "basketball", "baseball", "tennis", "cricket", "hockey", "football", "volleyball", "golf", "swimming", "running", "cycling", "boxing", "wrestling", "skiing", "surfing", "skating", "gymnastics", "archery", "rowing"],
@@ -42,14 +34,16 @@ const themes = {
     christmas: ["snowman", "reindeer", "santa", "elves", "presents", "tree", "stocking", "ornaments", "chimney", "holiday", "carols", "wreath", "mistletoe", "gingerbread", "northpole", "sleigh", "frosty", "rudolph", "garland", "jingle"]
 };
 
+// Function to scramble the letters of a word
 function scrambleWord(word) {
-    let scrambled = word.split('').sort(() => 0.5 - Math.random()).join('');
+    let scrambled = word.split('').sort(() => Math.random() - 0.5).join('');
     while (scrambled === word) {
-        scrambled = word.split('').sort(() => 0.5 - Math.random()).join('');
+        scrambled = word.split('').sort(() => Math.random() - 0.5).join('');
     }
     return scrambled;
 }
 
+// Function to display scrambled words on the page
 function displayScrambledWords(theme) {
     const tableBody = document.getElementById('words-table');
     tableBody.innerHTML = '';
@@ -68,6 +62,7 @@ function displayScrambledWords(theme) {
     });
 }
 
+// Function to check the user's answers
 function checkAnswer(theme) {
     const result = document.getElementById('result');
     let correctAnswers = 0;
@@ -80,10 +75,11 @@ function checkAnswer(theme) {
     result.textContent = `You got ${correctAnswers} out of ${themes[theme].length} correct!`;
 }
 
+// Function to run when the page loads
 window.onload = () => {
     const path = window.location.pathname;
     const page = path.split('/').pop().split('.')[0];
-    if (page in themes) {
+    if (themes[page]) {
         displayScrambledWords(page);
     }
 };
